05347393aa641e0c4220308f9b47c15ffb3c4810
Louise <144322022+Lousip@users.noreply.github.com>
2023-11-13 16:12:01 +0100

    Update PPPcr.py

--- PPPcr.py
+++ PPPcr.py
@@ -4,12 +4,10 @@ import csv
 
 
 # Set up which files are the samples and which is the housekeeping gene 
-assay_files = [r"C:\Users\file"]
-normaliser_files = [r"C:\Users\file"]
-
-# Put the xls files in a string variable
-assays = qpcr.read(assay_files)
-normalisers = qpcr.read(normaliser_files)
+assay_files = input("Name of assay file: ")
+open(assay_files, 'r')
+normaliser_files = input("Name of normaliser file: ")
+open(normaliser_files, 'r')
 
 # Put the xls files in a string variable
 assays = qpcr.read(assay_files)
@@ -17,7 +15,6 @@ normalisers = qpcr.read(normaliser_files)
 
 
 reader = qpcr.DataReader()
-
 assays = reader.read(assay_files)
 normalisers = reader.read(normaliser_files)
 
@@ -28,9 +25,9 @@ normalisers = qpcr.delta_ct(normalisers)
 # compute normalisation ddCT
 results = qpcr.normalise(assays, normalisers)
 print(results)
-# visualise the plot and save it in the format you need
+# visualise the plot and save it in png
 fig = results.preview()
-
+fig.savefig("results_pcr.png")
 
 # To save the datas to a new csv file
 results_csv = [results]
@@ -38,12 +35,9 @@ results_csv = [results]
 # Input names of your normaliser and assay to output name of your new file
 nameassay = input("name gene assay: ")
 namenorm = input("name gene normaliser: ")
-name.csv = nameassay + "ddct" + namenorm + ".csv"
-with open (name.csv,'w',newline = '') as csvfile:
+name_csv = nameassay + "ddct" + namenorm + ".csv"
+with open (name_csv,'w',newline = '') as csvfile:
     my_writer = csv.writer(csvfile, delimiter = ' ')
     my_writer.writerows(results_csv)
-
-
-
 
 
